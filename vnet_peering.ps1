02fd687fc8b1534430d06591de18baf245868879
Alex <shevashokc@gmail.com>
2020-06-22 21:18:29 +0300

    Create vnet_peering.ps1

---
+++ vnet_peering.ps1
@@ -0,0 +1,10 @@
+﻿#ENV
+$vnet_availability = "cs-vnet-availability"
+$vnet_infraservices = "cs-vnet-infraservices"
+
+
+# Peer VNet1 to VNet2.
+Add-AzVirtualNetworkPeering -Name 'AV_2_INFRA' -VirtualNetwork $vnet_availability -RemoteVirtualNetworkId $vnet_infraservices.id
+
+# Peer VNet2 to VNet1.
+Add-AzVirtualNetworkPeering -Name 'INFRA_2_AV' -VirtualNetwork $vnet_infraservices -RemoteVirtualNetworkId $vnet_availability.id
